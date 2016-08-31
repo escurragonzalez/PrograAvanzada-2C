2d5d6de17b1f85043286e23d6a29bebcb7db9b34
escurragonzalez <escurra.gonzalez@gmail.com>
2016-08-31 16:53:27 -0300

    Update Matriz

--- Practica/TDA/test/ar/com/unlam/test/MatrizMathTest.java
+++ Practica/TDA/test/ar/com/unlam/test/MatrizMathTest.java
@@ -2,17 +2,15 @@ package ar.com.unlam.test;
 
 import java.io.FileNotFoundException;
 
-import org.junit.Ignore;
+import org.junit.Test;
 
 import ar.com.unlam.tda.MatrizMath;
 
 public class MatrizMathTest {
 
-	@Ignore
+	@Test
 	public void toStringTest() throws FileNotFoundException {
-		MatrizMath mat = new MatrizMath("pruebas/pruebaMAtriz.in");
+		MatrizMath mat = new MatrizMath("pruebas/pruebaMatriz.in");
 		System.out.println(mat);
 	}
-	
-	
 }
--- Practica/TDA/test/ar/com/unlam/test/VectorMathTest.java
+++ Practica/TDA/test/ar/com/unlam/test/VectorMathTest.java
@@ -6,6 +6,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import ar.com.unlam.tda.VectorMath;
+import junit.framework.Assert;
 
 public class VectorMathTest {
 
@@ -14,9 +15,44 @@ public class VectorMathTest {
 		VectorMath vec = new VectorMath("pruebas/pruebaVector.in");
 		System.out.println(vec);
 	}
-	
+
+	@SuppressWarnings("deprecation")
 	@Test
-	public void sumaDeVectoresTest(){
-		
+	public void sumaDeVectoresTest() throws FileNotFoundException {
+		VectorMath v = new VectorMath("pruebas/pruebaVector.in");
+		VectorMath v2 = new VectorMath("pruebas/pruebaVector.in");
+		VectorMath v3 = new VectorMath(5);
+		double comp[] = { 2, 2, 10, 10, 6 };
+		v3.setComponentes(comp);
+		Assert.assertEquals(v3, v.sumar(v2));
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void restaDeVectoresTest() throws FileNotFoundException {
+		VectorMath v = new VectorMath("pruebas/pruebaVector.in");
+		VectorMath v2 = new VectorMath("pruebas/pruebaVector.in");
+		VectorMath v3 = new VectorMath(5);
+		double comp[] = { 0, 0, 0, 0, 0 };
+		v3.setComponentes(comp);
+		Assert.assertEquals(v3, v.restar(v2));
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void productoEscalarDeVectoresTest() throws FileNotFoundException {
+		VectorMath v = new VectorMath("pruebas/pruebaVector.in");
+		VectorMath v2 = new VectorMath("pruebas/pruebaVector.in");
+		Assert.assertEquals(61.0, v.productoEscalar(v2));
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void productoPorUnEscalarTest() throws FileNotFoundException {
+		VectorMath v = new VectorMath("pruebas/pruebaVector.in");
+		VectorMath v3 = new VectorMath(5);
+		double comp[] = { 2, 2, 10, 10, 6 };
+		v3.setComponentes(comp);
+		Assert.assertEquals(v3, v.productoPorUnEscalar(2));
 	}
 }
