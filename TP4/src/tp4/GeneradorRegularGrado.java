bb978370c0f4631c4399541a281c37b92f0a035c
escurragonzalez <escurra.gonzalez@gmail.com>
2016-10-26 19:28:44 -0300

    Generador Regular calculo de grado y porcentaje de Adyacencia

--- TP4/src/tp4/GeneradorRegularGrado.java
+++ TP4/src/tp4/GeneradorRegularGrado.java
@@ -1,8 +1,5 @@
 package tp4;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class GeneradorRegularGrado extends Generador {
 
 	private int grado;
@@ -13,27 +10,40 @@ public class GeneradorRegularGrado extends Generador {
 		generarMatriz();
 	}
 
+	public void calcularGrado() {
+		setGradoMinima(this.grado);
+		setGradoMaximo(this.grado);
+	}
+
+	public void calcularPorcentajeAdyacencia(){
+		double porcAdy=0;
+		porcAdy=this.grado/(double)(this.getCantNodos()-1);
+		setPorcAdyacencia(porcAdy);
+	}
+
 	private void generarMatriz() {
 
 		int recorrido = grado / 2;
 		int salto = 1;
+		int cont=0;
 
-		if (grado % 2 == 0)
+		if (grado % 2 == 0) {
 			for (int i = 0; i < recorrido; i++) {
 				for (int j = 0; j < getCantNodos() - 1; j++) {
 					if (j + salto < getCantNodos())
 						getMatrizDeAdyacencia().setValor(j, j + salto, true);
 					else
 						getMatrizDeAdyacencia().setValor(j, i - 1, true);
+					cont++;
 				}
-				getMatrizDeAdyacencia().setValor(getCantNodos() - salto,
-						salto - 1, true);
+				getMatrizDeAdyacencia().setValor(getCantNodos() - salto, salto - 1, true);
 				salto++;
-
+				cont++;
 			}
-		else {
+		} else {
 
 		}
+		setCantAristas(cont);
 	}
 
 }
