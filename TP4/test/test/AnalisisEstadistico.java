20067f5be2eb6a39941fce82752306d6f147b08e
Segovia Elías <segoviaelias@hotmail.com>
2016-11-15 13:16:50 -0300

    actulizacion

--- TP4/test/test/AnalisisEstadistico.java
+++ TP4/test/test/AnalisisEstadistico.java
@@ -6,12 +6,13 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import tp4.GeneradorAleatorioPorcentaje;
+import tp4.GeneradorRegularPorcentaje;
 import tp4.GrafoNDNP;
 
 public class AnalisisEstadistico {
 
 	@Test
-	public void secuenciaAleatoria() throws FileNotFoundException {
+	public void secuenciaAleatoriaParaGrafoAleatorio() throws FileNotFoundException {
 		int nodos = 600;
 		double adyacencia = 0.4;
 		int cantMenorColor = (int) Double.POSITIVE_INFINITY;
@@ -35,7 +36,7 @@ public class AnalisisEstadistico {
 	}
 
 	@Ignore
-	public void welshPowell() throws FileNotFoundException {
+	public void welshPowellParaGrafoAleatorio() throws FileNotFoundException {
 		int nodos = 600;
 		double adyacencia = 0.4;
 		int cantMenorColor = (int) Double.POSITIVE_INFINITY;
@@ -59,12 +60,84 @@ public class AnalisisEstadistico {
 	}
 
 	@Ignore
-	public void matula() throws FileNotFoundException {
+	public void matulaParaGrafoAleatorio() throws FileNotFoundException {
 		int nodos = 600;
 		double adyacencia = 0.4;
 		int cantMenorColor = (int) Double.POSITIVE_INFINITY;
 		int corridaMenorColor = 0;
 		GeneradorAleatorioPorcentaje generador = new GeneradorAleatorioPorcentaje(nodos, adyacencia);
+
+		generador.escribirArchivo("coloreo.out");
+		GrafoNDNP coloreo = new GrafoNDNP("coloreo.out");
+
+		for (int i = 0; i < 1000; i++) {
+
+			coloreo.matula();
+			System.out.println(coloreo.getCantColores());
+			if (cantMenorColor > coloreo.getCantColores()) {
+				cantMenorColor = coloreo.getCantColores();
+				corridaMenorColor = i + 1;
+			}
+		}
+		System.out.println("Menor Cantidad de color: " + cantMenorColor);
+		System.out.println("En la corrida: " + corridaMenorColor);
+	}
+
+	@Ignore
+	public void secuenciaAleatoriaParaGrafoRegular() throws FileNotFoundException {
+		int nodos = 1000;
+		double adyacencia = 0.5;
+		int cantMenorColor = (int) Double.POSITIVE_INFINITY;
+		int corridaMenorColor = 0;
+		GeneradorRegularPorcentaje generador = new GeneradorRegularPorcentaje(nodos, adyacencia);
+
+		generador.escribirArchivo("coloreo.out");
+		GrafoNDNP coloreo = new GrafoNDNP("coloreo.out");
+
+		for (int i = 0; i < 500; i++) {
+
+			coloreo.SecuenciaAleatorio();
+			System.out.println(coloreo.getCantColores());
+			if (cantMenorColor > coloreo.getCantColores()) {
+				cantMenorColor = coloreo.getCantColores();
+				corridaMenorColor = i + 1;
+			}
+		}
+		System.out.println("Menor Cantidad de color: " + cantMenorColor);
+		System.out.println("En la corrida: " + corridaMenorColor);
+	}
+
+	@Ignore
+	public void welshPowellParaGrafoRegular() throws FileNotFoundException {
+		int nodos = 1000;
+		double adyacencia = 0.5;
+		int cantMenorColor = (int) Double.POSITIVE_INFINITY;
+		int corridaMenorColor = 0;
+		GeneradorRegularPorcentaje generador = new GeneradorRegularPorcentaje(nodos, adyacencia);
+
+		generador.escribirArchivo("coloreo.out");
+		GrafoNDNP coloreo = new GrafoNDNP("coloreo.out");
+
+		for (int i = 0; i < 500; i++) {
+
+			coloreo.welshPowell();
+			System.out.println(coloreo.getCantColores());
+			if (cantMenorColor > coloreo.getCantColores()) {
+				cantMenorColor = coloreo.getCantColores();
+				corridaMenorColor = i + 1;
+			}
+		}
+		System.out.println("Menor Cantidad de color: " + cantMenorColor);
+		System.out.println("En la corrida: " + corridaMenorColor);
+	}
+
+	@Ignore
+	public void matulaParaGrafoRegular() throws FileNotFoundException {
+		int nodos = 1000;
+		double adyacencia = 0.5;
+		int cantMenorColor = (int) Double.POSITIVE_INFINITY;
+		int corridaMenorColor = 0;
+		GeneradorRegularPorcentaje generador = new GeneradorRegularPorcentaje(nodos, adyacencia);
 
 		generador.escribirArchivo("coloreo.out");
 		GrafoNDNP coloreo = new GrafoNDNP("coloreo.out");
